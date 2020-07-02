1ccb510f8a5ed0aa68907149724a4af3296cc509
Leslie G <lesliegonzalez@me.com>
2020-07-02 16:48:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,8 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
-def
+def batch_badge_creator(name)
 name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+end
+
+def 
