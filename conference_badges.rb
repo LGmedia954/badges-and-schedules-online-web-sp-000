ec0d92c0c41ec3c64598d96b9ba2121005ce8a58
Leslie G <lesliegonzalez@me.com>
2020-07-02 17:08:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ end
 def batch_badge_creator(speakers)
   speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   speakers.each do |badge|
-  puts "Meet Your Presenter #{badge}"
+  return "Meet Your Presenter #{badge}"
   end
 end
 
