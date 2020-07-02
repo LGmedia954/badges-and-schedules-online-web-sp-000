b57ca931556f90c46ce9836256936717edc04078
Leslie G <lesliegonzalez@me.com>
2020-07-02 18:16:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def assign_rooms(speakers)
 end
   
 def printer(speakers)
-  batch_badge_creator.each do |badge|
+  batch_badge_creator(speakers).each do |badge|
   puts badge
   end
   assign_rooms(speakers).each do |assignment|
